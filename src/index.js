498da213cd5a62ce6045079e274d181d87b81e50
zwmccall <32351629+zwmccall@users.noreply.github.com>
2022-02-02 18:22:13 -0600

    increased rows, refresh game on losing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import words from './words'
 import answers from './answers'
 
 const numOfFields = 5;
-const numOfRows = 3;
+const numOfRows = 6;
 const answer = answers[getRandomInt(2080)];
 console.log(answer);
 
@@ -72,6 +72,7 @@ const handleChange = e => {
               }
             } else {
               alert("Sorry you did not win, the correct answer was: " + answer);
+              window.location.reload(false);
             }
           }
         }
@@ -134,6 +135,27 @@ class Square extends React.Component {
             {this.renderSquare(12)}
             {this.renderSquare(13)}
             {this.renderSquare(14)}
+          </div>
+          <div className="board-row" name="row-4">
+            {this.renderSquare(15)}
+            {this.renderSquare(16)}
+            {this.renderSquare(17)}
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+          </div>
+          <div className="board-row" name="row-5">
+            {this.renderSquare(20)}
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+            {this.renderSquare(24)}
+          </div>
+          <div className="board-row" name="row-6">
+            {this.renderSquare(25)}
+            {this.renderSquare(26)}
+            {this.renderSquare(27)}
+            {this.renderSquare(28)}
+            {this.renderSquare(29)}
           </div>
         </div>
       );
